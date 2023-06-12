3dc9a5d6bbb50a775fb67bc12f5dcca60a6bab41
Christian Valentim <christianvalentim99@gmail.com>
2023-06-12 20:22:16 -0300

    Projeto Udemy: Calculadora

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.css'
+import Calculator from './components/Calculadora/Calculator';
+
+function App() {
+  return(
+    <div>
+      <h1>Calculadora</h1>
+      <Calculator></Calculator>
+    </div>
+  )
+}
+ 
+export default App
